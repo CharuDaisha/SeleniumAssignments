6bd7da7264d04e0145a8cadc0c6db59ef6ed7c70
CharuDaisha <50914191+CharuDaisha@users.noreply.github.com>
2022-06-16 10:39:23 -0700

    Update PrintDuplicatesInArray.java

--- week1/day2/classroom/arrays/PrintDuplicatesInArray.java
+++ week1/day2/classroom/arrays/PrintDuplicatesInArray.java
@@ -21,7 +21,16 @@ public class PrintDuplicatesInArray {
 					
 			// Out of the inner loop, check and print the first array variable if count is more than 0
 			
-			
+			int [] arr = new int [] {14,12,13,11,15,14,18,16,17,19,18,17,20};   
+    
+    System.out.println("Duplicate elements in given array: ");  
+    //Searches for duplicate element  
+    for(int i = 0; i < arr.length; i++) {  
+        for(int j = i + 1; j < arr.length; j++) {  
+            if(arr[i] == arr[j])  
+                System.out.println(arr[j]);  
+	}
+    }
 		
 	}
 
