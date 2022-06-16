b5f7a1cbfea84ab469fa10743ec4523c978cd974
CharuDaisha <50914191+CharuDaisha@users.noreply.github.com>
2022-06-16 10:12:32 -0700

    Update PrimeNumber.java

--- week1/day1/assignments/PrimeNumber.java
+++ week1/day1/assignments/PrimeNumber.java
@@ -76,7 +76,26 @@ public class PrimeNumber {
 
 			
 
-			// Print 'Not a Prime' when the condition doesn't match 
+			// Print 'Not a Prime' when the condition doesn't match
+		
+		  int i,m=0,flag=0;      
+		  int n=5;//it is the number to be checked    
+		  m=n/2;      
+		  if(n==0||n==1){  
+		   System.out.println(n+" is not prime number");      
+		  }else{  
+		   for(i=2;i<=m;i++){      
+		    if(n%i==0){      
+		     System.out.println(n+" is not prime number");      
+		     flag=1;      
+		     break;      
+		    }      
+		   }      
+		   if(flag==0)  { System.out.println(n+" is prime number"); }  
+	
+	
+	
+}
 
    }
 
